c412a1703d4e8296c91c8ca148b346cb46f5c8ea
Nicolas Torres <nicolasparratorres@gmail.com>
2025-05-24 02:29:20 -0500

    pagina

--- routes/routes.py
+++ routes/routes.py
@@ -12,7 +12,7 @@ router = APIRouter()
 
 @router.post("/noticias/{pagina}",status_code=status.HTTP_201_CREATED)
 async def crear_noticias(pagina: int):
-    nuevos = scrap()
+    nuevos = scrap(pagina)
     if nuevos is None:
         raise HTTPException(status_code=500, detail="Error al insertar en la base de datos")
     return {"inserted": nuevos}
